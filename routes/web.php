0e1939cde82343e1097b423c0e620fe23a040f27
Tahmid Ferdous <62140352+tahmid-tf@users.noreply.github.com>
2025-05-12 23:20:46 +0600

    permissions implemented
    Laravel-permission 'Spatie' implemented

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,9 @@
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
+use Spatie\Permission\Models\Role;
+use Spatie\Permission\Models\Permission;
+
 Route::get('/', function () {
     return view('welcome');
 });
@@ -11,10 +14,39 @@ Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
 
+
+// --------------------------- Spatie test ---------------------------
+
+Route::get('role', function () {
+
+//    $role = Role::create(['name' => 'admin']);
+//    $permission = Permission::create(['name' => 'edit articles']);
+
+//    $user = auth()->user();
+
+//    $user->assignRole('admin'); // role
+//    $user->givePermissionTo('edit articles'); // permission
+//    Role::find(1)->givePermissionTo('edit articles'); // giving permission to roles
+
+//    if ($user->hasRole('admin')) {
+//        // allow access
+//    }
+//
+//    if ($user->can('edit articles')) {
+//        // allow editing
+//    }
+
+});
+
+
+// ----------------------------------- Profile update -----------------------------------
+
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
-require __DIR__.'/auth.php';
+// ----------------------------------- Auth Routes -----------------------------------
+
+require __DIR__ . '/auth.php';
