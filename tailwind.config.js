dd7027cd1dda14ed8c40a97fc3186b9efe730f52
Tahmid Ferdous <62140352+tahmid-tf@users.noreply.github.com>
2025-05-12 22:18:34 +0600

    authentication implemented
    laravel breeze authentication implemented

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,14 +1,12 @@
 import defaultTheme from 'tailwindcss/defaultTheme';
+import forms from '@tailwindcss/forms';
 
 /** @type {import('tailwindcss').Config} */
 export default {
-    content: [
-        './vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php',
-        './storage/framework/views/*.php',
-        './resources/**/*.blade.php',
-        './resources/**/*.js',
-        './resources/**/*.vue',
-    ],
+    content: ['./vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php', './storage/framework/views/*.php', './resources/views/**/*.blade.php',],
+
+    // it will toggle the dark mode
+    darkMode: 'class',
     theme: {
         extend: {
             fontFamily: {
@@ -16,5 +14,6 @@ export default {
             },
         },
     },
-    plugins: [],
+
+    plugins: [forms],
 };
