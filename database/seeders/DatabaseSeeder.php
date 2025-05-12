d2b71d8bd7972beb486cba0daefc771ac1948fc6
Tahmid Ferdous <62140352+tahmid-tf@users.noreply.github.com>
2025-05-12 23:52:32 +0600

    Seeder data implemented

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,8 +3,12 @@
 namespace Database\Seeders;
 
 use App\Models\User;
+
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+use Spatie\Permission\Models\Permission;
+use Spatie\Permission\Models\Role;
 
 class DatabaseSeeder extends Seeder
 {
@@ -13,11 +17,44 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory(10)->create();
+        /*
 
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
+        1. Creating a user first
+        2. Creating all necessary roles
+        3. Creating all necessary permissions
+        4. Assigning role to user
+        5. Assigning permission to user
+
+         */
+
+        $this->create_user_roles_permission_and_assign_roles_and_permissions();
+    }
+
+    public function create_user_roles_permission_and_assign_roles_and_permissions(): void
+    {
+        $user = User::create([
+            'name' => 'Tahmid Ferdous',
+            'email' => 'tahmid.tf1@gmail.com',
+            'password' => Hash::make('123456789'),
         ]);
+
+        // role creation
+
+        $roles = ['admin', 'hr', 'manager'];
+
+        foreach ($roles as $role) {
+            Role::create(['name' => $role]);
+        }
+
+        // permission creation
+
+        Permission::create([
+            'name' => 'access_all',
+        ]);
+
+        // assigning role and permission to user
+
+        $user->assignRole('admin');
+        $user->givePermissionTo('access_all');
     }
 }
