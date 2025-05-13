14d81c1a56d2c1b7fd39c9439d31cdccf4d42b09
Tahmid Ferdous <62140352+tahmid-tf@users.noreply.github.com>
2025-05-13 17:04:33 +0600

    HM-4 : Sidebar creation and logic implementation

---
+++ resources/views/components/sidebar/hr.blade.php
@@ -0,0 +1,29 @@
+@if(auth()->user()->hasRole('hr'))
+
+    <a
+        class="nav-link collapsed"
+        href="javascript:void(0);"
+        data-bs-toggle="collapse"
+        data-bs-target="#collapseFlows"
+        aria-expanded="false"
+        aria-controls="collapseFlows"
+    >
+        <div class="nav-link-icon"><i data-feather="repeat"></i></div>
+        Employees
+        <div class="sidenav-collapse-arrow">
+            <i class="fas fa-angle-down"></i>
+        </div>
+    </a>
+    <div
+        class="collapse"
+        id="collapseFlows"
+        data-bs-parent="#accordionSidenav"
+    >
+        <nav class="sidenav-menu-nested nav">
+            <a class="nav-link" href=""
+            >Multi-Tenant Registration</a
+            >
+        </nav>
+    </div>
+
+@endif
