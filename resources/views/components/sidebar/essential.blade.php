c74d4963f58db8bb897e1be7c5b04a385550a4e4
Tahmid Ferdous <62140352+tahmid-tf@users.noreply.github.com>
2025-05-25 14:53:26 +0600

    HM-51 Salary structure sidebar implementation for hr and admin

--- resources/views/components/sidebar/essential.blade.php
+++ resources/views/components/sidebar/essential.blade.php
@@ -1,31 +1,70 @@
-    {{--  ------------------------------------------- Export -------------------------------------------  --}}
+{{--  ------------------------------------------- Payroll & Salary Management -------------------------------------------  --}}
+
+<div class="sidenav-menu-heading">Payroll & Salary Management</div>
+
+{{--  --------------------- Salary Structure ---------------------  --}}
+
+
+@if(auth()->user()->hasRole('admin') || auth()->user()->hasRole('hr'))
 
     <a
         class="nav-link collapsed"
         href="javascript:void(0);"
         data-bs-toggle="collapse"
-        data-bs-target="#collapseExport"
+        data-bs-target="#collapseStructure"
         aria-expanded="false"
-        aria-controls="collapseExport"
+        aria-controls="collapseStructure"
     >
         <div class="nav-link-icon"><i data-feather="repeat"></i></div>
-        Export
+        Salary Structure
         <div class="sidenav-collapse-arrow">
             <i class="fas fa-angle-down"></i>
         </div>
     </a>
     <div
         class="collapse"
-        id="collapseExport"
+        id="collapseStructure"
         data-bs-parent="#accordionSidenav"
     >
         <nav class="sidenav-menu-nested nav">
-
-            <a class="nav-link" href="{{ route('attendance_list_export') }}"
-            >Attendance Data Export</a
-            >
+            <a class="nav-link" href="{{ route('salary-structure.create') }}">Add/Edit Info</a>
+            <a class="nav-link" href="{{ route('salary-structure.index') }}">View All</a>
         </nav>
     </div>
 
+@endif
 
-    {{--  ------------------------------------------- Export -------------------------------------------  --}}
+{{--  ------------------------------------------- Payroll & Salary Management -------------------------------------------  --}}
+
+
+{{--  ------------------------------------------- Export -------------------------------------------  --}}
+
+<a
+    class="nav-link collapsed"
+    href="javascript:void(0);"
+    data-bs-toggle="collapse"
+    data-bs-target="#collapseExport"
+    aria-expanded="false"
+    aria-controls="collapseExport"
+>
+    <div class="nav-link-icon"><i data-feather="repeat"></i></div>
+    Export
+    <div class="sidenav-collapse-arrow">
+        <i class="fas fa-angle-down"></i>
+    </div>
+</a>
+<div
+    class="collapse"
+    id="collapseExport"
+    data-bs-parent="#accordionSidenav"
+>
+    <nav class="sidenav-menu-nested nav">
+
+        <a class="nav-link" href="{{ route('attendance_list_export') }}"
+        >Attendance Data Export</a
+        >
+    </nav>
+</div>
+
+
+{{--  ------------------------------------------- Export -------------------------------------------  --}}
