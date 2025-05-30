c1ad758f61731825d1873a4aeedb6382f383b9f4
Tahmid Ferdous <62140352+tahmid-tf@users.noreply.github.com>
2025-05-31 02:23:29 +0600

    HM-74 Expense approve and disapprove option and also selectable multiple [Bulk operation]

--- routes/essential/expense_management/expense_management.php
+++ routes/essential/expense_management/expense_management.php
@@ -1,11 +1,16 @@
 <?php
 
 use App\Http\Controllers\ExpenseManagement\ExpenseController;
+use App\Models\Expense;
 use Illuminate\Support\Facades\Route;
 
 Route::middleware(['auth', 'admin_hr'])->prefix('expense')->group(function () {
 
 // ---------------------------------------- Expense routes ----------------------------------------
-   Route::resource('expenses', ExpenseController::class);
+
+    Route::resource('expenses', ExpenseController::class);
+
+    Route::post('expenses_data/bulk_accept', [ExpenseController::class,'bulkAccept'])->name('expenses.bulkAccept');
+    Route::post('expenses_data/bulk_reject', [ExpenseController::class,'bulkReject'])->name('expenses.bulkReject');
 });
 
