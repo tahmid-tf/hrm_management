26a051485a8a26e15ff0d2c1df1b7b42a504bfb3
Tahmid Ferdous <62140352+tahmid-tf@users.noreply.github.com>
2025-06-04 02:45:11 +0600

    HM-90 two steps verification implemented with queue

--- app/Models/User.php
+++ app/Models/User.php
@@ -2,7 +2,8 @@
 
 namespace App\Models;
 
-// use Illuminate\Contracts\Auth\MustVerifyEmail;
+use App\Notifications\QueuedVerifyEmail;
+use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
@@ -10,7 +11,7 @@ use Spatie\Permission\Traits\HasRoles;
 use Laravel\Sanctum\HasApiTokens;
 use Illuminate\Database\Eloquent\SoftDeletes;
 
-class User extends Authenticatable
+class User extends Authenticatable implements MustVerifyEmail
 {
     /** @use HasFactory<\Database\Factories\UserFactory> */
     use HasFactory, Notifiable, HasApiTokens, HasRoles, SoftDeletes;
@@ -25,6 +26,12 @@ class User extends Authenticatable
 //        'email',
 //        'password',
 //    ];
+
+    public function sendEmailVerificationNotification(): void
+    {
+        $this->notify(new QueuedVerifyEmail);
+    }
+
 
     protected $guarded = [];
 
